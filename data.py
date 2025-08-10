935412e9c060447a4b57a28a5752aabdcdc3f20d
andy <lupolenkoandre@gmail.com>
2025-08-10 17:00:15 +0300

    add data

---
+++ data.py
@@ -0,0 +1,3 @@
+curs_url = 'https://stepik.org/lesson/'# сюда записываем уже первый шаг, нужного курса
+user_login = 'use@mail.ru'# Ваш логин на степик
+user_password = 'user_password'# Ваш пароль на степик
